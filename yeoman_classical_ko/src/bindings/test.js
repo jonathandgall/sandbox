52c0efe41502eeba8f07c2b163c9c28c51f862aa
Jonathan Gall <odajay@gmail.com>
2017-06-10 20:56:43 +0200

    Including content of the yo ko-classical and renaming folder appropriately.

---
+++ yeoman_classical_ko/src/bindings/test.js
@@ -0,0 +1,13 @@
+define(['knockout'], function(ko) {
+    ko.bindingHandlers.test = {
+        init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+            // This will be called when the binding is first applied to an element
+            // Set up any initial state, event handlers, etc. here
+        },
+        update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+            // This will be called once when the binding is first applied to an element,
+            // and again whenever any observables/computeds that are accessed change
+            // Update the DOM element based on the supplied values here.
+        }
+    };
+});
